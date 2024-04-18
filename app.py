fa9cf44401e0916b60a0f0dd7e6816864321f19b
Danny McAteer <mcateerd2@gmail.com>
2024-04-17 21:43:41 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from langchain import hub
+from langchain.agents import AgentExecutor, create_openai_tools_agent 
+from openai import OpenAI
+
+st.title('Mirror')
+
+# Point to the local server
+llm = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        with st.spinner("Reasoning..."):
+            try:
+                stream = llm.chat.completions.create(
+                    model='hermes-2-pro',
+                    messages=[
+                            {"role": m["role"], "content": m["content"]}
+                            for m in st.session_state.messages
+                        ],
+                    stream=True,
+                )
+                response = st.write_stream(stream)
+                st.session_state.messages.append({"role": "assistant", "content": response})
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")

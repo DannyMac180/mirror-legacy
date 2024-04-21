7a9ebd647c4ac4b91dd6c49359240200ef789534
Danny McAteer <mcateerd2@gmail.com>
2024-04-20 21:47:20 -0400

    switch to chatModel

--- lang_programs.py
+++ lang_programs.py
@@ -6,33 +6,45 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 
-llm = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio", model="bartowski/Meta-Llama-3-8B-Instruct-GGUF/Meta-Llama-3-8B-Instruct-fp16.gguf")
+chat = ChatOpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio", model="bartowski/Meta-Llama-3-8B-Instruct-GGUF/Meta-Llama-3-8B-Instruct-fp16.gguf")
 
-loader = ObsidianLoader(
-    path="/Users/danielmcateer/Library/Mobile Documents/iCloud~md~obsidian/Documents/Ideaverse"
-)
+response =chat.invoke(
+    [
+        HumanMessage(
+            content="What is the capital of France?"
+            )
+        ]
+    )
 
-obsidian_docs = loader.load()
+print(response.content)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(obsidian_docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+# loader = ObsidianLoader(
+#     path="/Users/danielmcateer/Library/Mobile Documents/iCloud~md~obsidian/Documents/Ideaverse"
+# )
 
-# Retrieve and generate using the relevant snippets of the blog.
-retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
+# obsidian_docs = loader.load()
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+# splits = text_splitter.split_documents(obsidian_docs)
+# vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+# # Retrieve and generate using the relevant snippets of the blog.
+# retriever = vectorstore.as_retriever()
+# prompt = hub.pull("rlm/rag-prompt")
+
+# def format_docs(docs):
+#     return "\n\n".join(doc.page_content for doc in docs)
 
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+# rag_chain = (
+#     {"context": retriever | format_docs, "question": RunnablePassthrough()}
+#     | prompt
+#     | llm
+#     | StrOutputParser()
+# )
 
-print(rag_chain.invoke("What is my wife's name based on my notes?"))
+# print(rag_chain.invoke("What is my wife's name based on my notes?"))
+

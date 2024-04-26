7cc760bab615e300bbfec75877f69fd247eac8e3
Danny McAteer <mcateerd2@gmail.com>
2024-04-25 20:10:56 -0400

    switch to chroma vectorstore

--- vector_store.py
+++ vector_store.py
@@ -1,35 +1,30 @@
-import weaviate
-from langchain_weaviate.vectorstores import WeaviateVectorStore
-from langchain.text_splitter import CharacterTextSplitter
+# import
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import ObsidianLoader
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-loader = ObsidianLoader(path="/Users/danielmcateer/Library/Mobile Documents/iCloud~md~obsidian/Documents/Ideaverse")
+# load the document and split it into chunks
+loader = ObsidianLoader(path=os.getenv("OBSIDIAN_PATH"))
 documents = loader.load()
+
+# split it into chunks
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings()
+# create the open-source embedding function
+embedding_function = OpenAIEmbeddings()
 
-weaviate_client = weaviate.connect_to_wcs(
-    cluster_url=os.getenv("WCS_URL"),
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_API_KEY"))
-)
-db = WeaviateVectorStore.from_documents(
-    documents=docs,
-    embedding=embeddings,
-    client=weaviate_client
-)
+# load it into Chroma
+db = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db")
 
-query = "What is the worldview of the writer according to the documents?"
+# query it
+query = "What does Andrej Karpathy say about the Transformer architecture?"
 docs = db.similarity_search(query)
 
-
-# Print the first 100 characters of each result
-for i, doc in enumerate(docs):
-    print(f"\nDocument {i+1}:")
-    print(doc.page_content[:100] + "...")
+# print results
+print(docs[0].page_content)

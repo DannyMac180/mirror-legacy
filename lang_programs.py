d00e6b6fd748b875ea3280e74e4e4acb5e7a9db1
Danny McAteer <mcateerd2@gmail.com>
2024-04-25 22:18:50 -0400

    working retriever doing vector simlarity search with chroma

--- lang_programs.py
+++ lang_programs.py
@@ -1,12 +1,20 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import SystemMessage, HumanMessage
 from contextlib import contextmanager
 from langchain.memory import ChatMessageHistory
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
 
 class LangChainProgram:
     def __init__(self):
         self.chat = None
         self.memory = ChatMessageHistory()
+        self.retriever = self.load_retriever()
+        
+    def load_retriever(self):
+        embeddings = OpenAIEmbeddings()
+        retriever = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+        return retriever
         
     @contextmanager
     def create_chat(self):
@@ -22,9 +30,11 @@ class LangChainProgram:
     def invoke_chat(self, message):
         with self.create_chat() as chat:
             self.memory.add_user_message(message)
+            docs = self.retriever.similarity_search(message, k=5)
             messages = [
                 SystemMessage(content="You are a helpful, smart, kind, and efficient AI assistant."),
-                *self.memory.messages
+                *self.memory.messages,
+                *[HumanMessage(content=doc.page_content) for doc in docs]
             ]
             response = ""
             for chunk in chat.stream(messages):
